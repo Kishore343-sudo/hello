2ef5222ad969edd69a864972a713ebfc160443fa
Kishore343-sudo <kkishorekumart97@gmail.com>
2024-09-16 14:45:23 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,11 +2,11 @@
 from flask import Flask
 
 #Welcome 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return 'Welcome to my web application!'
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host="127.0.0.1",port=8080,debug=True)
